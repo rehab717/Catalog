d813402d451b006584e5eb323695f0509e66f4bc
mo.rehab <mo.rehab@scandiweb.com>
2023-11-07 01:06:10 +0400

    removing unnecessary code

--- app/code/Scandiweb/Test/registration.php
+++ app/code/Scandiweb/Test/registration.php
@@ -1,5 +1,4 @@
-<?php \Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Scandiweb_Test',
-	__DIR__
-);
+<?php 
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Scandiweb_Test', __DIR__);
+
